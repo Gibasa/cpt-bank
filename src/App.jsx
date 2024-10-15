d48794e8cda8d61823e888315cccc6cccd10327a
Gilberto Sá <gibasa@gmail.com>
2024-10-15 17:02:04 -0300

    changed to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/theme";
 import Header from "@/layout/Header";
