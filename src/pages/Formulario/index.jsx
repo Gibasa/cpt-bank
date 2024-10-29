3d2f349530d54c66f087308036e166736a0b1088
Gilberto Sá <gibasa@gmail.com>
2024-10-29 13:24:35 -0300

    formulario updated with img and text.

--- src/pages/Formulario/index.jsx
+++ src/pages/Formulario/index.jsx
@@ -2,13 +2,43 @@ import styled from "styled-components";
 
 const FormularioStyled = styled.div`
   width: 100%;
-  max-width: 800px; /* Largura máxima para centralizar o formulário */
-  margin: 150px auto 0;
+  margin: 150px 0;
+  display: flex;
+
+  .CTAFormulario {
+    width: 40%;
+    margin-top: 50px;
+    margin-left: 9vw;
+    img {
+      width: 30vw;
+    }
+    h2 {
+      font-family: ${({ theme }) => theme.fonts.primary};
+      color: ${({ theme }) => theme.colors.primary};
+      font-size: 3vw;
+    }
+    p{
+      font-family: ${({ theme }) => theme.fonts.primary};
+      color: ${({ theme }) => theme.colors.primary};
+      font-size: 1.3vw;
+      margin-bottom: 50px;
+    }
+  }
 `;
 
 function Formulario() {
   return (
     <FormularioStyled>
+      <div className="CTAFormulario">
+        <h2>
+          Personalize a <br/>sua experiência.
+        </h2>
+        <p>Temos a solução perfeita para você</p>
+        <img
+          src="/images/solucoes-img.png"
+          alt="mulher sentada no computador com café na mão"
+        />
+      </div>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSeO5CwrPDEGiiC9ZzTBvGt6OXoHkeyib45UPa-JIcbMu4fkYw/viewform?embedded=true"
         width="100%"
